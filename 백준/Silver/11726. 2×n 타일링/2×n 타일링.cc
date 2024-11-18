2bafd7333eae38645c1ac74aa3558e4ca968a3da
eastlight82 <144019166+eastlight82@users.noreply.github.com>
2024-11-19 01:12:51 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int n; cin >> n;
+  vector <int> dp(n + 1);
+  dp[1] = 1; dp[2] = 2;
+  
+  for (int i = 3; i <= n; i++)
+  {
+    dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+  }
+
+  cout << dp[n];
+}
