bd4b64f611e0e37a96b5b11a5f6c88cd544592b4
Dongyoon Chang <144019166+eastlight82@users.noreply.github.com>
2026-04-08 18:52:13 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 676 ms, Memory: 157956 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -2,78 +2,76 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static StringBuilder[] graph;
-    static int n, m;
-    static boolean[][][] vis; //0: xbroke, 1: broke
+  static int n,m,ans=-1;
+  static StringBuilder[] graph;
+  static boolean[][][] vis; 
+  static int[] dx= {0,0,1,-1}; static int[] dy= {1,-1,0,0};
+  static class Node {
+    int y, x, cnt, destroyed; //1: 부숨
+    Node(int y, int x, int cnt, int destroyed){
+      this.y=y; this.x=x; this.cnt=cnt; this.destroyed=destroyed;
 
-    static int[][] dXY= {{0,1},{0,-1},{1,0},{-1,0}};
-    static int minDist= Integer.MAX_VALUE;
+    }
+  }
 
-    static class Coordinate{
-        int x, y, dist;
-        boolean broke;
+  public static void main(String[] args) throws Exception {
+    BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+    //1,1->n,m, 1벽(1개까지 부심)
+    StringTokenizer st= new StringTokenizer(br.readLine());
+    n= Integer.valueOf(st.nextToken());
+    m= Integer.valueOf(st.nextToken());
+    graph= new StringBuilder[n];
+    vis= new boolean[n][m][2];
 
-        Coordinate(int x, int y, int dist, boolean broke){
-            this.x=x; this.y=y; this.dist=dist; this.broke=broke;
-        }
+    for (int i = 0; i < n; i++) {
+      String brs= br.readLine();
+      graph[i]= new StringBuilder();
+      graph[i].append(brs);
     }
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    bfs(0,0);
+    System.out.println(ans);
+  }
 
-        //graph n*m(0:이동o, 1:x) //1,1->n,m을 min로
-        //1개까지 부수기o
-        StringTokenizer st= new StringTokenizer(br.readLine());
-        n= Integer.valueOf(st.nextToken()); m= Integer.valueOf(st.nextToken());
-        graph= new StringBuilder[n];
-        vis= new boolean[n][m][2];
+  static void bfs(int y, int x) {
+    Queue<Node> q= new LinkedList<>();
 
-        for (int i = 0; i < n; i++){
-            String brs= br.readLine();
-            graph[i]= new StringBuilder(brs);
-        }
+    q.offer(new Node(0,0,1,0));
+    while (!q.isEmpty()) {
+      Node cur=q.poll();
+      if (cur.y==n-1 && cur.x==m-1) {
+        ans=cur.cnt;
+        return;
+      }
+      
+      for (int i = 0; i < 4; i++) {
+        int nxtY= cur.y+dy[i]; int nxtX= cur.x+dx[i];
 
-        bfs();
-    }
-
-    static void bfs(){
-        Queue<Coordinate> q= new LinkedList<>();
-        q.offer(new Coordinate(0,0, 1, false));
-
-        while (!q.isEmpty()){
-            Coordinate cur= q.poll(); //cur.x cur.y
-            if (cur.x==(n-1) && cur.y==(m-1)){
-                System.out.println(cur.dist); //vis여부 저장해서 바로 출력
-                return;
+        if (nxtY>=0 && nxtY<n && nxtX>=0 && nxtX<m) {
+          //0일때 destroy ox
+          if (graph[nxtY].charAt(nxtX)=='0') {
+            if (cur.destroyed==0 && !vis[nxtY][nxtX][0]) {
+              q.offer(new Node(nxtY,nxtX,cur.cnt+1,0));
+              vis[nxtY][nxtX][0]=true;
             }
-
-            for (int i = 0; i < 4; i++) {
-                int nX= cur.x+dXY[i][0]; int nY= cur.y+dXY[i][1]; int nxtDist= cur.dist+1;
-
-                if (nX>=0 && nX<n && nY>=0 && nY<m){
-                    if (graph[nX].charAt(nY)=='0'){
-                        if (!cur.broke && !vis[nX][nY][0]){
-                            q.offer(new Coordinate(nX,nY,nxtDist,false));
-                            vis[nX][nY][0]=true;
-                        }
-                        else if (cur.broke && !vis[nX][nY][1]){
-                            q.offer(new Coordinate(nX,nY,nxtDist,true));
-                            vis[nX][nY][1]=true;
-                        }
-                    }
-                    else {
-                        if (!cur.broke){
-                            q.offer(new Coordinate(nX,nY,nxtDist,true));
-//                            vis[nX][nY][1]=true;
-                        }
-
-                    }
-                }
+            
+            else if (cur.destroyed==1 && !vis[nxtY][nxtX][1]) {
+              q.offer(new Node(nxtY,nxtX,cur.cnt+1,1));
+              vis[nxtY][nxtX][1]=true;
             }
+          }
 
+          else {
+            if (cur.destroyed==0 && !vis[nxtY][nxtX][0]) {
+              //graph 바꾸지 말기(벽도 0처럼)
+              q.offer(new Node(nxtY,nxtX,cur.cnt+1,1));
+              vis[nxtY][nxtX][1]=true;
+            }
+          }
         }
-        System.out.println(-1);
+      }
     }
 
+  }
 
 }
